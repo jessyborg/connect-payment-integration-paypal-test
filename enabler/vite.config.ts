18bc5cc5fc37fb5099fcb2ea565170c103006422
Johnson Ogwuru <johnson.ogwuru@commercetools.com>
2024-03-15 12:28:07 +0100

    fix(jwt): processor to accept configuration (#63)
    Co-authored-by: Danilo Delizia <danilo.delizia@commercetools.com>

--- enabler/vite.config.ts
+++ enabler/vite.config.ts
@@ -1,5 +1,5 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
