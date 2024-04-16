5ecaad2830e71b774b3254479e6668ad5481fefc
King-Hin Leung <kinghin.leung@commercetools.com>
2024-04-16 15:34:09 +0200

    Update config.ts

--- processor/src/config/config.ts
+++ processor/src/config/config.ts
@@ -1,23 +1,23 @@
 export const config = {
   // Required by Payment SDK
-  projectKey: process.env.CTP_PROJECT_KEY || 'payment-integration',
-  clientId: process.env.CTP_CLIENT_ID || 'xxx',
-  clientSecret: process.env.CTP_CLIENT_SECRET || 'xxx',
-  jwksUrl: process.env.CTP_JWKS_URL || 'https://mc-api.europe-west1.gcp.commercetools.com/.well-known/jwks.json',
-  jwtIssuer: process.env.CTP_JWT_ISSUER || 'https://mc-api.europe-west1.gcp.commercetools.com',
-  authUrl: process.env.CTP_AUTH_URL || 'https://auth.europe-west1.gcp.commercetools.com',
-  apiUrl: process.env.CTP_API_URL || 'https://api.europe-west1.gcp.commercetools.com',
-  sessionUrl: process.env.CTP_SESSION_URL || 'https://session.europe-west1.gcp.commercetools.com/',
+  projectKey: process.env.CTP_PROJECT_KEY,
+  clientId: process.env.CTP_CLIENT_ID,
+  clientSecret: process.env.CTP_CLIENT_SECRET,
+  jwksUrl: process.env.CTP_JWKS_URL,
+  jwtIssuer: process.env.CTP_JWT_ISSUER,
+  authUrl: process.env.CTP_AUTH_URL,
+  apiUrl: process.env.CTP_API_URL,
+  sessionUrl: process.env.CTP_SESSION_URL,
   healthCheckTimeout: parseInt(process.env.HEALTH_CHECK_TIMEOUT || '5000'),
 
   // Required by logger
   loggerLevel: process.env.LOGGER_LEVEL || 'info',
 
   // Update with specific payment providers config
-  paypalClientId: process.env.PAYPAL_CLIENT_ID || 'xxx',
-  paypalClientSecret: process.env.PAYPAL_CLIENT_SECRET || 'xxxx',
+  paypalClientId: process.env.PAYPAL_CLIENT_ID,
+  paypalClientSecret: process.env.PAYPAL_CLIENT_SECRET,
   paypalEnvironment: process.env.PAYPAL_ENVIRONMENT || 'test',
-  paypalWebhookId: process.env.PAYPAL_WEBHOOK_ID || '',
+  paypalWebhookId: process.env.PAYPAL_WEBHOOK_ID,
 
   // TODO review these configurations
   // supportedUIElements: convertStringCommaSeparatedValuesToArray(process.env.SUPPORTED_UI_ELEMENTS),
